924635e7cbc6c2c93d5988589953d2808fbb862c
Piotr Chojnacki <chojnacki.piotr91@gmail.com>
2019-09-08 19:13:44 +0200

    react project with hooks and one custom hook

---
+++ src/components/Ingredients/IngredientList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './IngredientList.css';
+
+const IngredientList = React.memo(props => {
+
+  const onRemoveItem = (ig) => {
+    props.onRemoveItem(ig)
+  }
+
+  return (
+    <section className="ingredient-list">
+      <h2>Loaded Ingredients</h2>
+      <ul>
+        {props.ingredients.map(ig => (
+          <li key={ig.id} onClick={() => onRemoveItem(ig.id)}>
+            <span>{ig.title}</span>
+            <span>{ig.amount}x</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+});
+
+export default IngredientList;
